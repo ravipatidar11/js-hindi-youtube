dcfdd2dab397e1555ca9f2b09f6920a2b998b671
ravipatidar11 <ravipatidar1525@gmail.com>
2026-08-11 21:19:27 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score="33abc"
 
-console.log(typeof score);
-console.log(typeof (score))
+// console.log(typeof score);
+// console.log(typeof (score))
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // When convert to Number
 // "33" => 33
@@ -18,7 +18,7 @@ let valueLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(valueLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 // In Blooean
@@ -30,5 +30,50 @@ console.log(booleanIsLoggedIn)
 let someNumber = 10
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// ********************* Operations **************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**4);
+
+
+let str1 = "hello"
+let str2 = " Ravi"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+// console.log(+33);
+// console.log(33);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+// console.log(num1)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
